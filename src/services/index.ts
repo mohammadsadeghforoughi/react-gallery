369df21f04cbd2d12057606393c348b6c015ec46
Mohammad Foroughi <mohammadforoughi@Mohammads-MacBook-Pro.local>
2022-05-24 14:18:37 +0430

    add env

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://apimocha.com/bearbulltraders/api'
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 const instance = axios.create({
     baseURL: BASE_URL,
